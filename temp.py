6eea3b8fc86fc9e33c08b9b7aebea4cdf3c4fb66
Jeremy Drapeau <202269459@cegepsherbrooke.qc.ca>
2025-10-06 13:04:34 -0400

    fix pour le site du super C à la place du maxi

--- temp.py
+++ temp.py
@@ -6,29 +6,22 @@ from time import sleep
 
 def getAllPages(driver) -> list :
     pages = list()
-    driver.get("https://www.maxi.ca/fr")
+    driver.get("https://www.superc.ca/fr")
     driver.set_window_size(1280, 720)
-    sleep(5)
+    sleep(2)
     try :
-        driver.find_element(By.CSS_SELECTOR, "#onetrust-accept-btn-handler").click()
+        driver.find_element(By.CSS_SELECTOR, "#onetrust-reject-all-handler").click()
         sleep(2)
     except :
         pass
-    menu = driver.find_element(By.CSS_SELECTOR, "ul[class='flex flex-row items-center justify-start']")
-    menu = menu.find_element(By.TAG_NAME, "li")
-    menu.click()
-    all_buttons = driver.find_element(By.CSS_SELECTOR, "div[data-testid='iceberg-main-nav-l2-tabs-list']").find_elements(By.TAG_NAME, "button")
+    driver.find_element(By.CSS_SELECTOR, "#navigation-level1--aisles").click()
+    menu = driver.find_element(By.CSS_SELECTOR, "div[class='sub--nav--first']").find_element(By.TAG_NAME, "ul")
+    all_li = menu.find_elements(By.CSS_SELECTOR, "li")
+    sleep(2)
 
-    for i in all_buttons :
-        i.click()
-        current_content = driver.find_element(By.CSS_SELECTOR, "ul[data-testid='iceberg-main-nav-l3-content-list']")
-        all_pages = current_content.find_elements(By.TAG_NAME, "a")
-        for y in all_pages :
-            is_bold = False
-            for s in y.get_attribute("class").split(" ") :
-                if s == "font-bold" :
-                    is_bold = True
-            if not is_bold :
-                pages.append(y.get_attribute("href"))
+    all_li[0].find_element(By.CSS_SELECTOR, "button").click()
+    sleep(1)
+    for i in all_li :
+        i.find_element(By.CSS_SELECTOR, "button").click()
+        pages.append(driver.find_element(By.CSS_SELECTOR, ".aisles--subNav--lev2").find_element(By.TAG_NAME, "a").get_attribute("href"))
     return pages
-
